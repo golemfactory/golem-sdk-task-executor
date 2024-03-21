2b7e9704b3cd37abc4731bf0cc2edb76f06f8fdb
Marcin Gordel <marcin.gordel@gmail.com>
2024-03-21 11:51:27 +0100

    docs: updated web examples to show how to debug in browser (#54)

--- tests/cypress/ui/docs-example-transfer-json.cy.ts
+++ tests/cypress/ui/docs-example-transfer-json.cy.ts
@@ -1,6 +1,6 @@
 describe("Docs Examples Transfer JSON", () => {
   it("should transfer json to provider", () => {
-    cy.visit("/docs-example-transfer-json");
+    cy.visit("/upload-json-in-browser");
     cy.get("#YAGNA_API_BASEPATH").clear().type(Cypress.env("YAGNA_API_BASEPATH"));
     cy.get("#SUBNET_TAG").clear().type(Cypress.env("YAGNA_SUBNET"));
     cy.get("#PAYMENT_NETWORK").clear().type(Cypress.env("PAYMENT_NETWORK"));
